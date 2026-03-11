416f5e42df7b2be748da1e45d98b08d4aeb3c2e3
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-03-11 20:08:41 +0700

    Initial commit

---
+++ src/main/java/com/example/Tugas_CRUD_20230140006/TugasCrud20230140006Application.java
@@ -0,0 +1,13 @@
+package com.example.Tugas_CRUD_20230140006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140006Application.class, args);
+	}
+
+}
