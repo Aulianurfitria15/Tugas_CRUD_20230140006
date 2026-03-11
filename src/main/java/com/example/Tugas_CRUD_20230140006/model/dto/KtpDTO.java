28cb8792f5b4b3c7536a8b6b7bc6cdd6be605da0
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-03-11 20:49:05 +0700

    membuat KtpDTO.java

---
+++ src/main/java/com/example/Tugas_CRUD_20230140006/model/dto/KtpDTO.java
@@ -0,0 +1,15 @@
+package com.example.Tugas_CRUD_20230140006.model.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class KtpDTO {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
